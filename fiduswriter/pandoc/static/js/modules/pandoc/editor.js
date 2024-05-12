81830d490ce8d932420e1c7ea97fb724b21c3855
Johannes Wilm <mail@johanneswilm.org>
2024-05-12 14:33:02 +0200

    export formats textile, rtf, typst, tei, docbook, fiduswriter/fiduswriter#1276

--- fiduswriter/pandoc/static/js/modules/pandoc/editor.js
+++ fiduswriter/pandoc/static/js/modules/pandoc/editor.js
@@ -8,14 +8,18 @@ export class EditorPandoc {
 
         exportMenu.content.push(
             {
-                title: gettext("Markdown"),
+                title: gettext("DocBook"),
                 type: "action",
-                tooltip: gettext("Export the document to a markdown file."),
+                tooltip: gettext("Export the document to a DocBook file using pandoc."),
                 order: 5,
                 action: editor => {
                     import("./exporter").then(({PandocConversionExporter}) => {
                         const exporter = new PandocConversionExporter(
-                            "markdown",
+                            "docbook",
+                            "dbk",
+                            "application/xml",
+                            false,
+                            false,
                             editor.getDoc({changes: "acceptAllNoInsertions"}),
                             editor.mod.db.bibDB,
                             editor.mod.db.imageDB,
@@ -25,8 +29,122 @@ export class EditorPandoc {
                         exporter.init()
                     })
                 }
-            }
-
+            },
+            {
+                title: gettext("Markdown"),
+                type: "action",
+                tooltip: gettext("Export the document to a markdown file using pandoc."),
+                order: 5,
+                action: editor => {
+                    import("./exporter").then(({PandocConversionExporter}) => {
+                        const exporter = new PandocConversionExporter(
+                            "markdown",
+                            "md",
+                            "text/markdown",
+                            true,
+                            false,
+                            editor.getDoc({changes: "acceptAllNoInsertions"}),
+                            editor.mod.db.bibDB,
+                            editor.mod.db.imageDB,
+                            editor.app.csl,
+                            editor.docInfo.updated
+                        )
+                        exporter.init()
+                    })
+                }
+            },
+            {
+                title: gettext("Richtext"),
+                type: "action",
+                tooltip: gettext("Export the document to a richtext file using pandoc."),
+                order: 5,
+                action: editor => {
+                    import("./exporter").then(({PandocConversionExporter}) => {
+                        const exporter = new PandocConversionExporter(
+                            "rtf",
+                            "rtf",
+                            "application/rtf",
+                            false,
+                            true,
+                            editor.getDoc({changes: "acceptAllNoInsertions"}),
+                            editor.mod.db.bibDB,
+                            editor.mod.db.imageDB,
+                            editor.app.csl,
+                            editor.docInfo.updated
+                        )
+                        exporter.init()
+                    })
+                }
+            },
+            {
+                title: gettext("Textile"),
+                type: "action",
+                tooltip: gettext("Export the document to a textile file using pandoc."),
+                order: 5,
+                action: editor => {
+                    import("./exporter").then(({PandocConversionExporter}) => {
+                        const exporter = new PandocConversionExporter(
+                            "textile",
+                            "textile",
+                            "text/textile",
+                            false,
+                            false,
+                            editor.getDoc({changes: "acceptAllNoInsertions"}),
+                            editor.mod.db.bibDB,
+                            editor.mod.db.imageDB,
+                            editor.app.csl,
+                            editor.docInfo.updated
+                        )
+                        exporter.init()
+                    })
+                }
+            },
+            {
+                title: gettext("TEI Simple"),
+                type: "action",
+                tooltip: gettext("Export the document to a TEI Simple file using pandoc."),
+                order: 5,
+                action: editor => {
+                    import("./exporter").then(({PandocConversionExporter}) => {
+                        const exporter = new PandocConversionExporter(
+                            "tei",
+                            "xml", // "tei" is the format name, but the file extension is "xml"
+                            "application/xml",
+                            false,
+                            false,
+                            editor.getDoc({changes: "acceptAllNoInsertions"}),
+                            editor.mod.db.bibDB,
+                            editor.mod.db.imageDB,
+                            editor.app.csl,
+                            editor.docInfo.updated
+                        )
+                        exporter.init()
+                    })
+                }
+            },
+            {
+                title: gettext("Typst"),
+                type: "action",
+                tooltip: gettext("Export the document to a typst file using pandoc."),
+                order: 5,
+                action: editor => {
+                    import("./exporter").then(({PandocConversionExporter}) => {
+                        const exporter = new PandocConversionExporter(
+                            "typst",
+                            "typ",
+                            "application/octet-stream",
+                            false,
+                            false,
+                            editor.getDoc({changes: "acceptAllNoInsertions"}),
+                            editor.mod.db.bibDB,
+                            editor.mod.db.imageDB,
+                            editor.app.csl,
+                            editor.docInfo.updated
+                        )
+                        exporter.init()
+                    })
+                }
+            },
         )
     }
 
--- fiduswriter/pandoc/static/js/modules/pandoc/exporter.js
+++ fiduswriter/pandoc/static/js/modules/pandoc/exporter.js
@@ -1,36 +1,77 @@
-import {jsonPost} from "../common"
+import download from "downloadjs"
+
+import {jsonPost, get} from "../common"
 import {PandocExporter} from "../exporter/pandoc"
 import {createSlug} from "../exporter/tools/file"
 
-const FILE_EXTENSIONS = {
-    "markdown": "md",
-}
-
 
 export class PandocConversionExporter extends PandocExporter {
-    constructor(format, ...args) {
+    constructor(format, fileExtension, mimeType, includeBibliography, fullFileExport, ...args) {
         super(...args)
         this.format = format
+        this.fileExtension = fileExtension
+        this.mimeType = mimeType
+        this.includeBibliography = includeBibliography
+        this.fullFileExport = fullFileExport
     }
 
     createExport() {
         // send to pandoc on server, then send converted file to user.
         this.zipFileName = `${createSlug(this.docTitle)}.${this.format}.zip`
-        return jsonPost(
-            "/api/pandoc/export/",
-            {
-                text: JSON.stringify(this.conversion.json),
-                from: "json",
-                to: this.format,
+
+        return Promise.all(this.httpFiles.map(binaryFile =>
+            get(binaryFile.url).then(
+                response => response.blob()
+            ).then(
+                blob => new Promise((resolve, reject) => {
+                    const reader = new window.FileReader()
+                    reader.onerror = reject
+                    reader.onload = () => {
+                        resolve(reader.result)
+                    }
+                    reader.readAsDataURL(blob)
+                })
+            ).then(
+                base64Object => Promise.resolve({contents: base64Object.split("base64,")[1], filename: binaryFile.filename})
+            )
+        )).then(
+            binaryFiles => {
+                const files = this.textFiles.concat(binaryFiles).reduce(
+                    (acc, file) => {
+                        acc[file.filename] = file.contents
+                        return acc
+                    },
+                    {}
+                )
+                const hasBibliography = files.hasOwnProperty("bibliography.bib")
+                return jsonPost(
+                    "/api/pandoc/export/",
+                    {
+                        text: JSON.stringify(this.conversion.json),
+                        from: "json",
+                        to: this.format,
+                        standalone: true,
+                        files,
+                        bibliography: hasBibliography ? ["bibliography.bib"] : [],
+                        citeproc: hasBibliography,
+                    }
+                )
             }
         ).then(
             response => response.json()
         ).then(
             json => {
+                if (this.fullFileExport) {
+                    const blob = new window.Blob([json.output], {type: this.mimeType})
+                    return download(blob, `document.${this.fileExtension}`, this.mimeType)
+                }
                 this.textFiles.push({
-                    filename: `document.${FILE_EXTENSIONS[this.format]}`,
+                    filename: `document.${this.fileExtension}`,
                     contents: json.output,
                 })
+                if (!this.includeBibliography) {
+                    this.textFiles = this.textFiles.filter(file => file.filename !== "bibliography.bib")
+                }
                 return this.createDownload()
             }
         )
